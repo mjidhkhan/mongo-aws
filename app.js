cf22be249b966e5f0a8b8e6941cc3647b0bfc84f
MH Khan <majidhkhan@outlook.com>
2020-10-14 23:34:18 +0100

    init project

---
+++ app.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello world!'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({ port: 5000 })
+    .then(res => {
+    console.log(` Server running at ${res.url}`)
+})
