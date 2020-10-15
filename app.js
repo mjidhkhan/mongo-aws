d11f028699ddae700b3a598986c183535b5a55b0
MH Khan <majidhkhan@outlook.com>
2020-10-15 02:24:44 +0100

    updated

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 const db = require('dotenv').config();
 
-const Post = require('./models/Post');
-const User = require('./models/User');
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers');
 
 
-
-const typeDefs = gql`
-    type Post{
-        id: ID!,
-        title: String!,
-        body: String!,
-        username: String!,
-        creaqtedAt: String!
-    }
-    type Query {
-        getPosts: [Post]
-    }
-`
-
-const resolvers = {
-    Query: {
-        async getPosts() {
-            try {
-                const posts = await Post.find();
-                return posts;
-            } catch (error) {
-                throw new Error(error);
-            }
-        }
-    }
-}
 const server = new ApolloServer({
     typeDefs,
     resolvers
@@ -40,7 +13,7 @@ const server = new ApolloServer({
 
 mongoose.connect(db.parsed.DB, { useNewUrlParser: true , useUnifiedTopology: true})
     .then(() => {
-        console.log(`MongoDb connected ${db.parsed.DB}`)
+        console.log(`MongoDb connected `)
         return server.listen({ port: 5000 })
     })
         .then(res => {
