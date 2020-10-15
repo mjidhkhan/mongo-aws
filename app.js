d4a19f35069f7bfd5dedb6c6ba0e44da1b6cf187
MH Khan <majidhkhan@outlook.com>
2020-10-15 01:48:58 +0100

    add method for get Posts

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
+const db = require('dotenv').config();
+
+const Post = require('./models/Post');
+const User = require('./models/User');
+
 
 
 const typeDefs = gql`
+    type Post{
+        id: ID!,
+        title: String!,
+        body: String!,
+        username: String!,
+        creaqtedAt: String!
+    }
     type Query {
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello world!'
+        async getPosts() {
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch (error) {
+                throw new Error(error);
+            }
+        }
     }
 }
-
 const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
-server.listen({ port: 5000 })
-    .then(res => {
-    console.log(` Server running at ${res.url}`)
+mongoose.connect(db.parsed.DB, { useNewUrlParser: true , useUnifiedTopology: true})
+    .then(() => {
+        console.log(`MongoDb connected ${db.parsed.DB}`)
+        return server.listen({ port: 5000 })
+    })
+        .then(res => {
+        console.log(` Server running at ${res.url}`)
 })
